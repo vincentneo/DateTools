c145ccb735c5ff122cd4edcf079e91851c80395b
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2024-02-18 23:59:49 +0800

    Update Package.swift for modern use

--- Package.swift
+++ Package.swift
@@ -1,9 +1,15 @@
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    defaultLocalization: "en",
+    products: [
+        .library(name: "DateToolsSwift", targets: ["DateToolsSwift"])
+    ],
     targets: [
-        Target(name: "DateToolsSwift")
+        .target(name: "DateToolsSwift",
+                path: "DateToolsSwift/DateTools",
+                resources: [.copy("DateTools.bundle")])
     ]
 )
-package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
